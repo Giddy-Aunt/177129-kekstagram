9f4b35581d5dac2f9505c95aa142ac330e97a51f
giddy-aunt <sensitive-salaryman@yandex.ru>
2016-10-06 21:26:43 +0300

    1.2. Начинаем программировать

---
+++ src/js/check.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var getMessage = function(a, b) {
+  var message = 'Переданы некорректные данные';
+  switch(typeof a) {
+    case 'boolean':
+      if(a) {
+        message = 'переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+      } else {
+        message = 'Переданное GIF-изображение не анимировано';
+      };
+      break;
+    case 'number':
+      message = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
+      break;
+    case 'object':
+      if(typeof b === 'object') {
+        var artifactsSquare = 0;
+        for(var i = 0; i < a.length; i++) {
+          artifactsSquare += a[i] * b[i];
+        };
+        message = 'Общая площадь артефактов сжатия ' + artifactsSquare + ' пикселей';
+
+      } else {
+        var amountOfRedPoints = 0;
+        for(var j = 0; j < a.length; j++) {
+          amountOfRedPoints += a[j];
+        };
+
+        message = 'Количество красных точек во всех строчках изображения ' + amountOfRedPoints;
+      };
+      break;
+  };
+
+  return message;
+};
