e8e0e3aff9839b0e765f41487236b6664d629fd0
giddy-aunt <sensitive-salaryman@yandex.ru>
2016-10-11 19:13:09 +0300

    Задание 8, Canvas

--- src/js/resizer.js
+++ src/js/resizer.js
@@ -119,6 +119,34 @@
           this._resizeConstraint.side - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2);
 
+      //Отрисовка черного полупрозрачного слоя вокруг области кадрирования.
+
+      this._ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+
+      this._ctx.beginPath();
+      this._ctx.moveTo((-this._container.width / 2), (-this._container.height / 2));
+      this._ctx.lineTo((this._container.width / 2), (-this._container.height / 2));
+      this._ctx.lineTo((this._container.width / 2), (this._container.height / 2));
+      this._ctx.lineTo((-this._container.width / 2), (this._container.height / 2));
+      this._ctx.moveTo((-this._resizeConstraint.side / 2) - this._ctx.lineWidth, (-this._resizeConstraint.side / 2) - this._ctx.lineWidth);
+      this._ctx.lineTo((this._resizeConstraint.side / 2), (-this._resizeConstraint.side / 2) - this._ctx.lineWidth);
+      this._ctx.lineTo((this._resizeConstraint.side / 2), (this._resizeConstraint.side / 2));
+      this._ctx.lineTo((-this._resizeConstraint.side / 2) - this._ctx.lineWidth, (this._resizeConstraint.side / 2));
+      this._ctx.closePath();
+      this._ctx.fill('evenodd');
+
+      //Вывод размеров изображения.
+
+      var PADDING = 10;
+      var FONT_RATIO = 0.05;
+      var fontSize = this._resizeConstraint.side * FONT_RATIO;
+      this._ctx.font = fontSize + 'px Arial';
+      this._ctx.textBaseline = 'bottom';
+      this._ctx.textAlign = 'center';
+      this._ctx.fillStyle = '#ffffff';
+      this._ctx.fillText(this._image.naturalWidth + ' X ' + this._image.naturalHeight, 0, (-this._resizeConstraint.side / 2 - PADDING));
+
+
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
       // следующий кадр рисовался с привычной системой координат, где точка
